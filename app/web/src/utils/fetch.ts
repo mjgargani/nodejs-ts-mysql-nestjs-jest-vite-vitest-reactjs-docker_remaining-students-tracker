151b0193f2410e26e63f50aa3fe635675398c9e1
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2024-10-29 23:36:15 -0300

    feat: web env implementation

--- app/web/src/utils/fetch.ts
+++ app/web/src/utils/fetch.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'localhost';
+const API_PORT = import.meta.env.VITE_API_PORT || 3000;
+const API = ["http://", API_URL, ":", API_PORT].join("");
 
 const get = async () => await axios.get(API+"/students");
 const post = async (data: unknown) => await axios.post(API+"/students", data);
